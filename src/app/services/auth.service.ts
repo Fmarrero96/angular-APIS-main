12035e03404d4e074651e327b09debda1096f66b
Facundo Marrero <facu_marrero@hotmail.com>
2022-08-24 23:11:14 -0300

    manejo de headers

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment} from './../../environments/environment';
 import { Auth } from '../models/auth.model';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,14 @@ export class AuthService {
   }
 
 
-  profile(){
-    return this.http.get(`${this.apiUrl}/login`);
+  profile(token: string){
+    /*let headers = new HttpHeaders();
+    headers.set('Authorization', `Bearer ${token}`);*/ // funciona para cuando es mas dinamico
+    return this.http.get<User>(`${this.apiUrl}/profile`,{
+      headers: {
+        Authorization: `Bearer ${token}`,
+        //'content-type': 'aplication/token'
+      }
+    });
   }
 }
